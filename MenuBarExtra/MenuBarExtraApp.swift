86a159228d494a5f8ab93707b2040fea70285433
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2022-10-29 22:39:49 +0200

    Initial Commit

---
+++ MenuBarExtra/MenuBarExtraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuBarExtraApp.swift
+//  MenuBarExtra
+//
+//  Created by Michele Manniello on 29/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MenuBarExtraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
